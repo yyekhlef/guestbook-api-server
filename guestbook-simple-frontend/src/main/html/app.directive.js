77e23772c100de163a9c42ed34daf8e5ca405d44
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2016-03-20 10:19:12 +0100

    feat(frontend): use the new infos returned by the api

--- guestbook-simple-frontend/src/main/html/app.directive.js
+++ guestbook-simple-frontend/src/main/html/app.directive.js
@@ -4,19 +4,21 @@ redisApp.directive('app', function () {
     return {
         restrict: 'E',
         template: '<div style="width: 50%; margin-left: 20px">' +
-        '<h2>Guestbook</h2>' +
+        '<h2>Guestbook <i>(version {{app.version}})</i></h2>' +
         '<form>' +
         '<fieldset>' +
+        '<input ng-model="app.userName" placeholder="Username" class="form-control" type="text" name="input"><br>' +
         '<input ng-model="app.msg" placeholder="Messages" class="form-control" type="text" name="input"><br>' +
-        '<button type="button" class="btn btn-primary" ng-click="app.onRedis()">Submit</button>' +
+        '<button type="button" class="btn btn-primary" ng-click="app.storeInBackend()">Submit</button>' +
         '</fieldset>' +
         '</form>' +
         '<ul>' +
         '<li ng-repeat="msg in app.messages track by $index">' +
-        '{{msg.content}}' +
+        '{{msg.content}} ' +
+        '(publié par <b>{{msg.userName || "un inconnu"}}</b> <span class="text-info">datetime: {{msg.metadata.datetimeString}}, server: {{msg.metadata.apiServerName}}</span>)' +
         '</li>' +
         '</ul>' +
-        '<p><i>{{app.redisResponse}}</i></p>' +
+        '<p class="bg-danger"><i><b>{{app.backendResponse}}</b></i></p>' +
         '</div>',
         controller: AppController,
         controllerAs: 'app'
@@ -25,23 +27,40 @@ redisApp.directive('app', function () {
 
 function AppController($http) {
     this.messages = [{content: "si tu vois ce message c'est que ça n'a pas fonctionné :("}];
-    var self=this;
+    this.version = "unknown";
+    var self = this;
     $http.get("/api/v1/guestbook/messages")
-        .success(function (data, status) {
-            console.log("status code: " + status)
-            console.log("received " + data.length + " elements");
-            self.messages = data;
+        .then(function (response) {
+                console.log("[/guestbook/messages] status code: " + response.status);
+                console.log("[/guestbook/messages] received " + response.data.length + " elements");
+                self.messages = response.data;
+            },
+            function (response) {
+                console.log("[/guestbook/messages] status code: " + response.status);
+                self.backendResponse = "Something is wrong with the backend :(";
+            });
+
+    $http.get("/api/v1/version")
+        .then(function (response) {
+            console.log("[/version] status code: " + response.status);
+            self.version = response.data.version;
         });
 
-    this.onRedis = function () {
-        var data = {
-            content: this.msg
+    this.storeInBackend = function () {
+        console.log("storeInBackend: got content [" + this.msg + "] and userName[" + this.userName + "]");
+        var postBody = {
+            content: this.msg,
+            userName: this.userName
         };
-        this.messages.unshift(data);
         this.msg = "";
-        $http.post("/api/v1/guestbook/messages", data)
-            .success(angular.bind(this, function (data) {
-                this.redisResponse = "(Redis updated)";
-            }));
+        this.userName = "";
+        $http.post("/api/v1/guestbook/messages", postBody)
+            .then(angular.bind(this, function (data, status) {
+                    this.messages.unshift(data);
+                }), function (response) {
+                    console.log("[/guestbook/messages] status code: " + response.status);
+                    self.backendResponse = "Houston, something went wrong while storing to the backend";
+                }
+            );
     };
 }
