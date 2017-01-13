4c7c5413fab5dea89458fce2038cbaca46e57f9a
Christophe Furmaniak <christophe.furmaniak@gmail.com>
2017-01-13 11:01:40 +0100

    chore(os3): provide deployment files for openshift
    - works fine in minishift

--- guestbook-simple-frontend/src/main/scripts/startup.sh
+++ guestbook-simple-frontend/src/main/scripts/startup.sh
@@ -1,5 +1,14 @@
 #!/bin/ash
 
+echo "Testing runtime env"
+echo
+echo "content of /etc/nginx/html"
+ls /etc/nginx/html
+echo "------------------------------------"
+echo "content of /etc/nginx/html/config"
+ls /etc/nginx/html/config
+echo "------------------------------------"
+
 if /usr/local/bin/confd -onetime -backend env
   then
     echo
